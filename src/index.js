0b83e0ea12064dc6f05b2be53a27dbe8264f0eeb
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-06 21:12:07 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function submitHandler(){
+    const taskForm = document.querySelector('#create-task-form')
+    taskForm.addEventListener('submit', function(e){
+      e.preventDefault()
+      const form = e.target
+      const task = form["new-task-description"].value
+
+      const taskLi = document.createElement('li')
+      taskLi.textContent = task
+
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = 'x'
+      deleteButton.classList.add('delete')
+      
+      taskLi.append(deleteButton)
+
+      const taskList = document.querySelector('#tasks')
+      taskList.append(taskLi)
+
+      form.reset()
+    })
+  }
+    function clickHandler(){
+      document.addEventListener('click', function(e){
+        if(e.target.matches('.delete')){
+          e.target.parentElement.remove()
+        }
+      })
+    }
+    
+    submitHandler()
+    clickHandler()
 });
+
+
